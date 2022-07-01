39fd5c7ee1e71bef3110aeae43153f09dee07a09
Crynowecer0 <crynowecer11@gmail.com>
2022-07-01 18:20:35 -0400

    biolerplate and psuedocode

---
+++ script.js
@@ -0,0 +1,25 @@
+//rocks paper scissors game
+//establish counter for game winners
+//get input from the user
+    //only accept certain inputs
+//generate computer selection
+//game logic
+//tally who won
+//if tally >=5, display winner and reset
+
+
+
+
+
+
+//function skeletons
+function computerPlay() {
+//psuedocode
+}
+function playRound(playerSelection, computerSelection) {
+//psuedocode
+}
+
+function game() {
+
+}
